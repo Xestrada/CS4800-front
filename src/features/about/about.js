7da656e79fbf1567692153b7e79d6ed8dbfba102
Xestrada <Xestrada.Jorge@gmail.com>
2019-01-31 17:52:48 -0800

    Added info to About page

--- src/features/about/about.js
+++ src/features/about/about.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Table } from 'reactstrap';
 import Header from '../common/header';
 import './about.scss';
 
@@ -9,6 +10,42 @@ class About extends Component {
     return (
       <div className="about-root">
         <Header/>
+        <h2>Team Members:</h2>
+        <Table striped>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Joshua</td>
+            <td>Chen</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jorge</td>
+            <td>Estrada</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Wei-Hao</td>
+            <td>Lan</td>
+          </tr>
+          <tr>
+            <th scope="row">4</th>
+            <td>Issac</td>
+            <td>Kim</td>
+          </tr>
+        </tbody>
+      </Table>
+      <h2>GitHub Links:</h2>
+      <Table striped>
+        <tbody>
+          <tr>
+            <th scope="row">Frontend:</th>
+            <td>
+              <a href="https://github.com/Xestrada/CS4800-front">https://github.com/Xestrada/CS4800-front</a>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
       </div>
     );
   }
